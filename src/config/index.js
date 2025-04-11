e67e06038a83c890caf5d75e9372f5fcab14614e
Manuel Godoy <manuelgodoytorrealba@icloud.com>
2025-04-11 23:20:14 +0200

    upodate change

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     metaToken: process.env.META_TOKEN,
     phoneNumberId: process.env.PHONE_NUMBER_ID,
-    toNumber: process.env.TO_NUMBER,
+    toNumber: process.env.TO_NUMBER_1,
     // toNumber2: process.env.TO_NUMBER_2,
     goldApiKey: process.env.GOLD_API_KEY
 };
