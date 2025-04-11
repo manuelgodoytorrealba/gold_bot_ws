3318e94d057703ade5d3bac68711d79bd9c9e79d
Manuel Godoy <manuelgodoytorrealba@icloud.com>
2025-04-11 22:36:59 +0200

    adding other number

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-const { toNumber } = require('./config');
+const { toNumber, toNumber2 } = require('./config');
 const { sendWhatsappMessage } = require('./services/whatsappService');
 const { generateGoldReport } = require('./utils/generateGoldReport');
 
 async function main() {
   try {
     const templateParams = await generateGoldReport();
-    await sendWhatsappMessage(toNumber, templateParams);
+    await Promise.all([
+      sendWhatsappMessage(toNumber, templateParams),
+      sendWhatsappMessage(toNumber2, templateParams)
+    ]);
   } catch (error) {
     console.error('❌ Error al ejecutar el bot:', error.message);
   }
 }
 
+console.log('🚀 Iniciando bot...');
 main();
