960e5e834b7c25935f1aa344a32e9fb11275360b
Manuel Godoy <manuelgodoytorrealba@icloud.com>
2025-04-11 23:16:45 +0200

    update

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ module.exports = {
     metaToken: process.env.META_TOKEN,
     phoneNumberId: process.env.PHONE_NUMBER_ID,
     toNumber: process.env.TO_NUMBER,
-    toNumber2: process.env.TO_NUMBER_2,
+    // toNumber2: process.env.TO_NUMBER_2,
     goldApiKey: process.env.GOLD_API_KEY
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { toNumber, toNumber2 } = require('./config');
+const { toNumber} = require('./config');
 const { sendWhatsappMessage } = require('./services/whatsappService');
 const { generateGoldReport } = require('./utils/generateGoldReport');
 
@@ -7,7 +7,7 @@ async function main() {
     const templateParams = await generateGoldReport();
     await Promise.all([
       sendWhatsappMessage(toNumber, templateParams),
-      sendWhatsappMessage(toNumber2, templateParams)
+
     ]);
   } catch (error) {
     console.error('❌ Error al ejecutar el bot:', error.message);
