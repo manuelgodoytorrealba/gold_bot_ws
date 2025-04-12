0b2ea3faed2dc2f39a6135f0824255b290d722d9
Manuel Godoy <manuelgodoytorrealba@icloud.com>
2025-04-12 19:34:24 +0200

    updating how pupeteer manage this

--- src/services/bolivarExchangeRates/getParaleloRate.js
+++ src/services/bolivarExchangeRates/getParaleloRate.js
@@ -2,7 +2,11 @@ const puppeteer = require('puppeteer');
 
 async function getDolarParaleloRate() {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    });
+    
     const page = await browser.newPage();
 
     await page.goto('https://monitordolarvenezuela.com/', {
